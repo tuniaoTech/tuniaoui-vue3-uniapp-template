2b6e04a3e7f2631c1e99b487372bb3de88a8fc27
jaylen <350683609@qq.com>
2023-06-08 22:44:35 +0800

    删除部份依赖包 更新模板内容

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
   import { ref } from 'vue'
   import TnButton from '@tuniao/tnui-vue3-uniapp/components/button/src/button.vue'
-  const counter = ref(0)
+  import TnTabbar from '@tuniao/tnui-vue3-uniapp/components/tabbar/src/tabbar.vue'
+  import TnTabbarItem from '@tuniao/tnui-vue3-uniapp/components/tabbar/src/tabbar-item.vue'
+  import TnNotify from '@tuniao/tnui-vue3-uniapp/components/notify/src/notify.vue'
 
+  import type { TnNotifyInstance } from '@tuniao/tnui-vue3-uniapp'
+
+  const notifyRef = ref<TnNotifyInstance>()
+
+  const counter = ref(0)
   const addCounter = () => {
     counter.value++
+  }
+
+  const tabbarChangeEvent = (index: string | number) => {
+    notifyRef.value?.show({
+      msg: `当前选中的是第${index}个Tab`,
+    })
   }
 </script>
 
@@ -12,13 +25,18 @@
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
-      <view class="tn-mr"><TnIcon name="logo-tuniao" /></view>
       <view class="counter">Current Counter: {{ counter }}</view>
     </view>
     <view class="operation">
       <TnButton type="primary" @click="addCounter">Add Counter Value</TnButton>
     </view>
+    <TnTabbar fixed @change="tabbarChangeEvent">
+      <TnTabbarItem text="首页" icon="home" active-icon="home-fill" />
+      <TnTabbarItem text="我的" icon="my" active-icon="my-fill" />
+    </TnTabbar>
   </view>
+
+  <TnNotify ref="notifyRef" />
 </template>
 
 <style lang="scss" scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  base: './',
 })
